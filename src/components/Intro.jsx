5d0f6336e696dbf2affe2b38189f2ee4621c5ca3
Rajnivas <rajnivas.t@emsibg.com>
2023-03-12 15:57:33 +0530

    Updated the content-text and Image in Intro page

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,12 +3,18 @@ import React from "react";
 
 export default function Intro(){
     return(
-        <div id="Intro" className="container-fluid">
+        <div id="Intro" >
             <div id="Main">
-                <span>Hi I'm</span>
-                <h1>Rajniv</h1>
-                <div id="photo">
-                    <img src={process.env.PUBLIC_URL + "/Images/Rajnivas_photo.png" }  alt="Rajnivas.png"/>
+                <div id="content">
+                    <div id="content-text">
+                        <span>Hi I'm</span>
+                        <h1>Rajnivas Thiyagarajan</h1>
+                        
+                        <span>I am a full stack<br></br>webdeveloper &<br></br>Designer</span>
+                    </div>
+                    <div id="photo">
+                        <img src={process.env.PUBLIC_URL + "/Images/Rajnivas_photo.png" }  alt="Rajnivas.png"/>
+                    </div>
                 </div>
                 
             </div>
